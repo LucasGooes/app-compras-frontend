f047b8cca357f0c265367671164d6d37c1e00d5e
LucasGooes <lucasmarcos343@gmail.com>
2021-02-01 16:41:15 -0400

    Interceptor para incluir token nas requisições

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AuthInterceptor } from './../interceptors/auth-interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
@@ -13,6 +12,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage_service';
 import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -32,7 +32,7 @@ import { ClienteService } from '../services/domain/cliente.service';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    AuthInterceptor,
+    AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
--- src/interceptors/auth-interceptor.ts
+++ src/interceptors/auth-interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../config/api.config";
 import { StorageService } from "../services/storage_service";
 
 @Injectable()
@@ -11,11 +12,10 @@ export class AuthInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let localUser = this.storage.getLocalUser();
-        //let N = API_CONFIG.baseUrl.length;
-        //let requestToAPI = req.url.substring(0, N) == API_CONFIG.baseUrl;
-        if (localUser /*&& requestToAPI*/) {
-            console.log('passou aqui')
-            const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token) });
+        let N = API_CONFIG.baseUrl.length;
+        let requestToAPI = req.url.substring(0, N) == API_CONFIG.baseUrl;
+        if (localUser && requestToAPI) {
+            const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});
             return next.handle(authReq);
         } else {
             return next.handle(req);
