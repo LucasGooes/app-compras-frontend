8ed9303b9b95546e5652c1b83dd7225d7c8018cd
LucasGooes <lucasmarcos343@gmail.com>
2021-02-04 14:42:42 -0400

    Povoando dinamicamente estado e cidade

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
+import { EstadoDTO } from './../../models/estado.dto';
+import { EstadoService } from './../../services/domain/estado.service';
+import { CidadeService } from './../../services/domain/cidade.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CidadeDTO } from '../../models/cidade.dto';
 
 /**
  * Generated class for the SignupPage page.
@@ -17,10 +21,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
 
   formGroup: FormGroup;
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService) {
     this.formGroup = this.formBuilder.group({
       nome: ['Joaquin', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
       email: ['joaquin@gmail.com', [Validators.required, Validators.email]],
@@ -38,6 +46,26 @@ export class SignupPage {
       estadoId: [null, [Validators.required]],
       cidadeId: [null, [Validators.required]]
     });
+  }
+
+  ionViewDidLoad() {
+    this.estadoService.findAll()
+      .subscribe(response => {
+        this.estados = response;
+        this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+        this.updateCidades();
+      },
+      error => {});
+  }
+
+  updateCidades() {
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id)
+      .subscribe(response => {
+        this.cidades = response;
+        this.formGroup.controls.cidadeId.setValue(null);
+      },
+      error => {});
   }
 
   signupUser() {
